ddafba4968a2b5bf90faf644978618cff22e246c
shukabum <s_sharma2@me.iitr.ac.in>
2024-01-20 17:09:58 +0530

    leaderboard changes

--- src/Comp/Leaderboard.jsx
+++ src/Comp/Leaderboard.jsx
@@ -1,16 +1,38 @@
 import React from "react";
+import "../App.css";
 
 const Leaderboard = ({ leaderboard }) => {
   return (
     <div className="leaderboard">
       <h3>Leaderboard</h3>
-      <ol>
-        {leaderboard.map((entry, index) => (
-          <li key={index}>
-            {entry.student}: {entry.score}
-          </li>
-        ))}
-      </ol>
+      <table>
+        <thead>
+          <tr style={{
+            fontWeight:"900",
+            fontSize:"1.1rem",
+            
+
+          }}>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard.map((player, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{player.student}</td>
+              <td>{player.score}</td>
+
+            </tr>
+          ))}
+
+          
+
+        </tbody>
+      </table>
+      
     </div>
   );
 };
